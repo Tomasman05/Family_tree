a2e7ea0c40d96f047b95d975dc668fdb37a06087
Vito <lolozol80xd@gmail.com>
2024-01-12 10:45:59 +0100

    ongoing

--- src/app/family.service.ts
+++ src/app/family.service.ts
@@ -1,47 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { createConnection } from 'mysql2';
-import { Connection } from 'mysql2/typings/mysql/lib/Connection';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FamilyService {
-  private connection: Connection;
-  constructor() { 
-    this.connection= createConnection({
-      host:"",
-      user:"",
-      password:"",
-      database:""
-    })
-    this.connection.connect((err:any)=>{
-      if(err){
-        console.error("Can't connect to the database: ",err);
-        return;
-      }
-      console.log("Connected succesfully.")
-    })
-  }
-  getFamilyMembers(): Promise<any[]> {
-    return new Promise((resolve, reject) => {
-      const query = 'SELECT * FROM FamilyMembers';
-      this.connection.query(query, (err, results) => {
-        if (err) {
-          console.error('Error executing query:', err);
-          reject(err);
-        } else {
-          resolve(results);
-        }
-      });
-    });
-  }
-  closeConnection(): void {
-    this.connection.end((err) => {
-      if (err) {
-        console.error('Error closing database connection:', err);
-      } else {
-        console.log('Database connection closed');
-      }
-    });
+  private apiUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) {}
+
+  getFamilyMembers(): Observable<any> {
+    const url = `${this.apiUrl}/familytree`;
+    return this.http.get(url);
   }
 }
+
